09cc5b2d0815077f7ec74b9a36c066170c43d59d
Paulius Zubavičius <paulius@zubavicius.lt>
2019-09-25 09:06:04 +0300

    java types day 1

---
+++ l03_types/src/main/java/com/vcs/lects/l03/integers/JavaTypes.java
@@ -0,0 +1,37 @@
+package com.vcs.lects.l03.integers;
+
+
+
+
+public class JavaTypes {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println((byte) 131);
+		
+		
+	}
+	
+	
+	
+	private void integers() {
+		
+		int sk = 6;
+		
+		Integer sk2 = Integer.MAX_VALUE;
+		
+		sk2 = sk + sk2;
+		
+		sk2 = null;
+		
+	
+		
+		Double a = 100.0 + sk2;
+		
+		
+
+	}
+	
+
+}
