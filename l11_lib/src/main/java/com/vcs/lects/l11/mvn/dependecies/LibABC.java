ac8698392b7f96ac3465c9c278c2900c149ec631
Paulius Zubavičius <paulius@zubavicius.lt>
2019-10-07 11:34:43 +0300

    mvn dependencies

---
+++ l11_lib/src/main/java/com/vcs/lects/l11/mvn/dependecies/LibABC.java
@@ -0,0 +1,32 @@
+package com.vcs.lects.l11.mvn.dependecies;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class LibABC {
+
+	@Deprecated
+	public int sudetis(int sk1, int sk2) {
+
+		int suma = sk1 + sk2;
+
+		if (suma < 0) {
+			System.out.println("Negative value ");
+		}
+
+		return suma;
+	}
+
+	public int sudetis(String sk1, String sk2) {
+
+		if (StringUtils.isNumeric(sk1) && StringUtils.isNumeric(sk2)) {
+			int suma = Integer.parseInt(sk1) + Integer.parseInt(sk2);
+
+			if (suma < 0) {
+				System.out.println("Negative value ");
+			}
+		}
+		return 0;
+
+	}
+
+}
---
+++ l11_lib_myprj/src/main/java/com/vcs/lects/l11/myprj/MyPrj.java
@@ -0,0 +1,15 @@
+package com.vcs.lects.l11.myprj;
+
+import com.vcs.lects.l11.mvn.dependecies.LibABC;
+
+public class MyPrj {
+
+	public static void main(String[] args) {
+
+		LibABC abc = new LibABC();
+
+		System.out.println("Ats.: " + abc.sudetis("-4", "2"));
+
+	}
+
+}
