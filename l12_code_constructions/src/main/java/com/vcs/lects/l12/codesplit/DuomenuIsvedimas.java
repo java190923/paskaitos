72111567120d65aa3b3bd4870364169a7bba0b1a
Paulius Zubavičius <paulius@zubavicius.lt>
2019-10-08 11:18:27 +0300

    added method to isvedimas

--- l12_code_constructions/src/main/java/com/vcs/lects/l12/codesplit/DuomenuIsvedimas.java
+++ l12_code_constructions/src/main/java/com/vcs/lects/l12/codesplit/DuomenuIsvedimas.java
@@ -2,8 +2,14 @@ package com.vcs.lects.l12.codesplit;
 
 public class DuomenuIsvedimas {
 
-    
 
+    public void isvesti(String... lines) {
+        for (String line : lines) {
+            if (line != null && !line.isEmpty()) {
+                System.out.println(line);
+            }
+        }
+    }
 
 
 }
