a7b40b8670b00c401748f93364650b150f08c1d2
Paulius Zubavičius <paulius@zubavicius.lt>
2019-10-08 22:52:27 +0300

    reordering task test

---
+++ l09_generics_collections/src/main/java/com/vcs/lects/l09/tasks/IReordering.java
@@ -0,0 +1,14 @@
+package com.vcs.lects.l09.tasks;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IReordering {
+
+    /**
+     * @param mokiniai - isrikiuoti pagal vardus
+     * @return turetu grazinti isrikiuotus pagal pazymiu vidurki reziais
+     */
+    public List<List<Mokinys>> getReordered(Map<String, List<Mokinys>> mokiniai);
+}
+
