7337358060138889abe73d2d719363be1dbe6be5
Paulius Zubavičius <paulius@zubavicius.lt>
2019-10-08 11:31:48 +0300

    isvedimas main

---
+++ l12_code_constructions/src/main/java/com/vcs/lects/l12/codesplit/Main.java
@@ -0,0 +1,12 @@
+package com.vcs.lects.l12.codesplit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DuomenuIsvedimas di = new DuomenuIsvedimas();
+
+
+        di.isvesti("Pirma  eilute", "antra eilute", "", "ketvirta", null, "5");
+
+    }
+}
